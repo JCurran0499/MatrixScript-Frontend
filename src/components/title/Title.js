8ddd23046d004fb4f8c477116d3458ff2bc2ea0c
Jack Curran <jackurran49@gmail.com>
2023-04-12 11:10:01 -0400

    back to normal

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -3,7 +3,7 @@ import './Title.css'
 export const Title = () => {
     return (
         <div id="title">
-            <h1>MatrixScript (Hello World!)</h1>
+            <h1>MatrixScript</h1>
         </div>
     )
 }
